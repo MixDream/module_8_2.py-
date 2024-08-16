1911433804abd3c5f7a9aa9a6a92834661bb33c9
MixDream <max_juldashev@mail.ru>
2024-08-16 14:42:55 +0300

    FIX zadania.py

--- zadania.py
+++ zadania.py
@@ -9,12 +9,11 @@ def personal_sum(numbers):
       print(f"Некорректный тип данных для подсчёта суммы - {num}")
   return result, incorrect_data
 
+
 def calculate_average(numbers):
   try:
     sum, incorrect_data = personal_sum(numbers)
-    if incorrect_data > 0:
-      print("В numbers записан некорректный тип данных")
-      return None
+    # Удалили условие if incorrect_data > 0: return None
     if len(numbers) == 0:
       return 0
     return sum / len(numbers)
@@ -23,6 +22,7 @@ def calculate_average(numbers):
     return None
   except ZeroDivisionError:
     return 0
+
 print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
